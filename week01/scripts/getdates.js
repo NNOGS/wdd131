6fbad398d586e7ea9dae965dc7a7bd9c3e9960f2
NNOGS <nnogswill@gmail.com>
2025-11-17 17:36:13 +0100

    My Country Page

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,2 +1,23 @@
 // This is to automate the date below on the footer of the html
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+// let firstName = "Antonia";
+// let lastName = "Francesca";
+
+// // Using function declaration/definition syntax
+// function fullName(firstName, lastName) {
+//     return `${firstName} ${ lastName }`;
+// }
+
+// // Using function expression syntax
+// const fullName = function (firstName, lastName) {
+//     return `${firstName} ${ lastName }`;
+// }
+
+// // Using arrow function expression syntax
+// const fullName = (firstName, lastName) => `${firstName} ${lastName}`;
+
+// // an expression that calls the fullName function declaration and writes the result to an existing HTML element's text node with the ID of fullName.
+// document.getElementById("fullName").textContent = fullName(firstName, lastName);
+// // or
+// document.querySelector("#fullName").innerHTML = fullName(firstName, lastName);
